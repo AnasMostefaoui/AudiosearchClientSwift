4571a9dc08e13fc6584bbf45f48e7855a968bad4
Mostefaoui Mohamed Anes <m.anas@outlook.com>
2016-10-05 10:50:01 +0300

    update to swift 3

--- Audiosearch.swift
+++ Audiosearch.swift
@@ -10,9 +10,9 @@ import Foundation
 import Alamofire
 import p2_OAuth2
 
-public typealias ServiceResponseAny = (AnyObject?, NSError?) -> Void
+public typealias ServiceResponseAny = (Any?, Error?) -> Void
 
-public class Audiosearch {
+open class Audiosearch {
     
     var oauth2: OAuth2ClientCredentials?
     var settings: OAuth2JSON
@@ -45,157 +45,221 @@ public class Audiosearch {
         self.oauth2!.authorize()
     }
     
-    public func getShowById(id: Int, onCompletion: ServiceResponseAny ) -> Void {
+    open func getShowById(_ id: Int, onCompletion: @escaping ServiceResponseAny ) -> Void {
         let stringId = String(id)
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/shows/\(stringId)", encoding:.JSON).responseJSON {response in switch response.result {
-            case .Success(let JSON):
-                let result = JSON
-                onCompletion(result, nil)
-            case .Failure(let error):
-                onCompletion(nil, error)
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/shows/\(stringId)", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+ 
+                }
             }
-        }
     }
     
-    public func getShowBySearchString(words: String, onCompletion: ServiceResponseAny) -> Void {
-        let podcastWordsEncoded = words.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+    open func getShowBySearchString(_ words: String, onCompletion: @escaping ServiceResponseAny) -> Void {
+        let podcastWordsEncoded = words.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
     
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/search/shows/\(podcastWordsEncoded!)"
-            , encoding:.JSON).responseJSON {response in switch response.result {
-            case .Success(let JSON):
-                let result = JSON
-                onCompletion(result, nil)
-            case .Failure(let error):
-                onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/search/shows/\(podcastWordsEncoded!)"
+            , encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
-        
     }
 
-    public func getEpisodeById(epId: Int, onCompletion: ServiceResponseAny) -> Void {
+    open func getEpisodeById(_ epId: Int, onCompletion: @escaping ServiceResponseAny) -> Void {
         let stringID = String(epId)
         
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/episodes/\(stringID)", encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/episodes/\(stringID)", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
     
-    public func search(query: String, params: Dictionary<String,String>?, type: String, onCompletion: ServiceResponseAny) -> Void  {
-        var queryItems: [NSURLQueryItem] = []
-        let query:NSString = query.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+    open func search(_ query: String, params: Dictionary<String,String>?, type: String, onCompletion: @escaping ServiceResponseAny) -> Void  {
+        var queryItems: [URLQueryItem] = []
+        let query:NSString = query.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)! as NSString
         let queryString = "https://www.audiosear.ch/api/search/\(type)/\(query)"
-        let components = NSURLComponents(string: queryString)
+        var components = URLComponents(string: queryString)
         if params != nil {
             for (key, value) in params! {
-                queryItems.append(NSURLQueryItem(name: key , value: value as String))
+                queryItems.append(URLQueryItem(name: key , value: value as String))
             }
         }
         components?.queryItems = queryItems
         let finalSearchString = components!.string!
-        self.oauth2!.request(.GET, finalSearchString, encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, finalSearchString, encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
 
-    public func getTrending(onCompletion: ServiceResponseAny) -> Void {
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/trending", encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+    open func getTrending(_ onCompletion: @escaping ServiceResponseAny) -> Void {
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/trending", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
     
-    public func getTastemakers(type: String, number: Int, onCompletion: ServiceResponseAny) -> Void {
+    open func getTastemakers(_ type: String, number: Int, onCompletion: @escaping ServiceResponseAny) -> Void {
         let stringNum = String(number)
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/tastemakers/\(type)/\(stringNum)", encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/tastemakers/\(type)/\(stringNum)", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
     
-    public func getPersonById(id: Int, onCompletion: ServiceResponseAny) -> Void  {
+    open func getPersonById(_ id: Int, onCompletion: @escaping ServiceResponseAny) -> Void  {
         let stringId = String(id)
         print("https://www.audiosear.ch/api/people/\(stringId)")
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/people/\(stringId)", encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/people/\(stringId)", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
         
     }
     
-    public func getPersonByName(name: String, onCompletion: ServiceResponseAny) -> Void  {
-        let personNameEncoded = name.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+    open func getPersonByName(_ name: String, onCompletion: @escaping ServiceResponseAny) -> Void  {
+        let personNameEncoded = name.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/search/people/\(personNameEncoded!)"
-            , encoding:.JSON).responseJSON {response in switch response.result {
-            case .Success(let JSON):
-                print("Success with JSON: \(JSON)")
-                let result = JSON
-                onCompletion(result, nil)
-            case .Failure(let error):
-                onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/search/people/\(personNameEncoded!)"
+            , encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
     
-    public func getRelated(type: String, id: Int, onCompletion: ServiceResponseAny) -> Void {
+    open func getRelated(_ type: String, id: Int, onCompletion: @escaping ServiceResponseAny) -> Void {
         let stringId = String(id)
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/\(type)/\(stringId)/related", encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/\(type)/\(stringId)/related", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
     
-    public func getTastemakersBySource(type: String, number: Int, tasteMakerId: Int, onCompletion: ServiceResponseAny) -> Void {
+    open func getTastemakersBySource(_ type: String, number: Int, tasteMakerId: Int, onCompletion: @escaping ServiceResponseAny) -> Void {
         let stringTasteMakerID = String(tasteMakerId)
         let stringNum = String(number)
         
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/tastemakers/\(type)/source/\(stringTasteMakerID)/\(stringNum)", encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/tastemakers/\(type)/source/\(stringTasteMakerID)/\(stringNum)", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
     
-    public func getEpisodeSnippet(episodeId: Int, timestampInSecs: Int, onCompletion: ServiceResponseAny) -> Void {
+    open func getEpisodeSnippet(_ episodeId: Int, timestampInSecs: Int, onCompletion: @escaping ServiceResponseAny) -> Void {
         let stringEpId = String(episodeId)
         let stringTimestamp = String(timestampInSecs)
         
-        self.oauth2!.request(.GET, "https://www.audiosear.ch/api/episodes/\(stringEpId)/snippet/\(stringTimestamp)", encoding:.JSON).responseJSON {response in switch response.result {
-        case .Success(let JSON):
-            let result = JSON
-            onCompletion(result, nil)
-        case .Failure(let error):
-            onCompletion(nil, error)
-            }
+        self.oauth2!.request(.get, "https://www.audiosear.ch/api/episodes/\(stringEpId)/snippet/\(stringTimestamp)", encoding:JSONEncoding.default)
+            .responseJSON {response in
+                if response.result.isSuccess {
+                    if let json = response.result.value {
+                        onCompletion(json, nil)
+                    }
+                }
+                else{
+                    if let error = response.result.error {
+                        onCompletion(nil, error)
+                    }
+                    
+                }
         }
     }
         
@@ -203,23 +267,19 @@ public class Audiosearch {
 
 public extension OAuth2 {
     public func request(
-        method: Alamofire.Method,
-        _ URLString: URLStringConvertible,
+        _ method: HTTPMethod,
+        _ URLString: URLConvertible,
         parameters: [String: AnyObject]? = nil,
-        encoding: Alamofire.ParameterEncoding = .URL,
+        encoding: Alamofire.ParameterEncoding =  URLEncoding.default,
         headers: [String: String]? = nil)
-        -> Alamofire.Request
+        -> Alamofire.DataRequest
     {
-        
+ 
         var hdrs = headers ?? [:]
         if let token = accessToken {
             hdrs["Authorization"] = "Bearer \(token)"
         }
-        return Alamofire.request(
-            method,
-            URLString,
-            parameters: parameters,
-            encoding: encoding,
-            headers: hdrs)
+
+        return Alamofire.request(URLString, method: .get, parameters: parameters, encoding: JSONEncoding.default,headers:hdrs)
     }
 }
